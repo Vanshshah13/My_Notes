b214a4d5c89c9ace1eec14c52fd2ab64d1272910
Vanshshah13 <shahvansh848@gmail.com>
2025-09-25 11:27:25 +0530

    Initial Commit

---
+++ backend/config/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try{
+    const connectionInstance = await mongoose.connect(process.env.MONGO_URI)
+    console.log(`MONGODB CONNECTED : ${connectionInstance.connection.host}`)
+  }catch(err){
+    console.log(err);
+    process.exit(1);
+  }
+}
---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from "express"
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import authRoutes from "./routes/auth.routes.js"
+import notesRoutes from "./routes/notes.routes.js"
+import path from "path"
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/users" , authRoutes)
+app.use("/api/notes" , notesRoutes)
+
+const __dirname = path.resolve();
+
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname , "frontend" , "dist")));
+  app.get("/*splat" , (req , res) => {
+    res.sendFile(path.resolve(__dirname , "frontend" , "dist" , "index.html"))
+  })
+}
+
+connectDB();
+
+app.listen(port , (req , res) => {
+  console.log(`Server Listening at http://localhost:${port}`)
+})
